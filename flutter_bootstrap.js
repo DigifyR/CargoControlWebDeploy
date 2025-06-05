3a1ba303059864a83ba364b7967a9742ce0c37d4
UsmanAsad87 <usmanasad0324@gmail.com>
2025-06-05 06:15:00 +0500

    Web upadted

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "549938449"
+    serviceWorkerVersion: "2915389870"
   }
 });

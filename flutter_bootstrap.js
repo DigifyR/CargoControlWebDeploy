90e3e9abf785a6103b5e2bbe1f57e3bcf8c1e4ea
UsmanAsad87 <usmanasad0324@gmail.com>
2025-08-22 21:32:02 +0500

    Code Pushed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3551277771"
+    serviceWorkerVersion: "3496948716"
   }
 });
